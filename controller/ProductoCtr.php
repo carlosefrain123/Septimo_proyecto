5a3da294110a823f2c864747bc0a981b3b6426d2
carlosefrain123 <carlosefrainchallo1@gmail.com>
2023-12-22 19:09:33 -0500

    22/12/2022 - 19:09 - Se agrega para que se visualice en la consola el id del prod_id

--- controller/ProductoCtr.php
+++ controller/ProductoCtr.php
@@ -4,7 +4,7 @@ require_once("../models/ProductoMdl.php");
 $producto=new Producto();
 switch ($_GET['op']) {
     case 'guardar':
-        $producto->insert_producto($_POST['nombre_producto']);
+        $datos=$producto->insert_producto($_POST['nombre_producto']);
         if (empty($_FILES['file']['name'])) {
         }else{
             $countfiles=count($_FILES['file']['name']);
@@ -16,9 +16,13 @@ switch ($_GET['op']) {
             for ($index=0; $index < $countfiles; $index++) { 
                 $nombre=$_FILES['file']['tmp_name'][$index];
                 $destino=$ruta.$_FILES['file']['name'][$index];
+                /* foreach ($datos as $row) {
+                    $producto->insert_imagenes($row['prod_id'],$nombre);
+                } */
                 move_uploaded_file($nombre,$destino);
             }
         } 
+        echo json_encode($datos);
         break;    
 }
 ?>
--- models/ProductoMdl.php
+++ models/ProductoMdl.php
@@ -5,7 +5,20 @@ class Producto extends Conectar{
         parent::set_names();
         $sql=$conectar->prepare("INSERT INTO producto(prod_id,nombre_producto) VALUES(NULL,:nombre_producto)");   
         $sql->bindParam(':nombre_producto',$nombre_producto);
-        return $sql->execute();
+        $sql->execute();
+
+        $sql1="select last_insert_id() as 'prod_id';";//Devolver el ultimo ID
+        $sql1=$conectar->prepare($sql1);
+        $sql1->execute();
+        return $resultado=$sql1->fetchAll(pdo::FETCH_ASSOC);
     }
+   /*  public function insert_imagenes($prod_id,$imgd_nom){
+        $conectar=parent::Conexion();
+        parent::set_names();
+        $sql=$conectar->prepare("INSERT INTO producto(imgd_id,prod_id,imgd_nom) VALUES (null,:prod_id,:imgd_nom)");
+        $sql->bindParam(':prod_id',$prod_id);
+        $sql->bindParam(':imgd_nom',$imgd_nom);
+        return $sql->execute();
+    } */
 }
 ?>
