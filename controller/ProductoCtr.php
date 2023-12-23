4195c7813ea1a288d5a3a30bae5bd48dff17a2a1
carlosefrain123 <carlosefrainchallo1@gmail.com>
2023-12-23 06:35:05 -0500

    23/12/2023 - 06:34 - Se direcciona a la carpeta img y en ello conforme se crea, se sotiene el nombre de la id como carpeta

--- controller/ProductoCtr.php
+++ controller/ProductoCtr.php
@@ -1,28 +1,27 @@
 <?php
 require_once("../config/conexion.php");
 require_once("../models/ProductoMdl.php");
-$producto=new Producto();
+$producto = new Producto();
 switch ($_GET['op']) {
     case 'guardar':
-        $datos=$producto->insert_producto($_POST['nombre_producto']);
+        $datos = $producto->insert_producto($_POST['nombre_producto']);
         if (empty($_FILES['file']['name'])) {
-        }else{
-            $countfiles=count($_FILES['file']['name']);
-            $ruta="../assets/img";
-            $files_arr=array();
-            if (!file_exists($ruta)) {
-                mkdir($ruta, 0777, true);
-            }
-            for ($index=0; $index < $countfiles; $index++) { 
-                $nombre=$_FILES['file']['tmp_name'][$index];
-                $destino=$ruta.$_FILES['file']['name'][$index];
-                foreach ($datos as $row) {
-                    $producto->insert_imagenes($row['prod_id'],$_FILES['file']['name'][$index]);
+        } else {
+            foreach ($datos as $row) {
+                $countfiles = count($_FILES['file']['name']);
+                $ruta = "../assets/img/" . $row['prod_id'] . "/";
+                $files_arr = array();
+                if (!file_exists($ruta)) {
+                    mkdir($ruta, 0777, true);
                 }
-                move_uploaded_file($nombre,$destino);
+                for ($index = 0; $index < $countfiles; $index++) {
+                    $nombre = $_FILES['file']['tmp_name'][$index];
+                    $destino = $ruta . $_FILES['file']['name'][$index];
+                    $producto->insert_imagenes($row['prod_id'], $_FILES['file']['name'][$index]);
+                    move_uploaded_file($nombre, $destino);
+                }
             }
-        } 
+        }
         echo json_encode($datos);
-        break;    
+        break;
 }
-?>
